c6c0fe4bf79d55964682deffc84356482af0201e
vanshkhandekar <148205080+vanshkhandekar@users.noreply.github.com>
2025-08-13 17:27:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,7 @@ OWNER_ID = 8351290270
 OWNER_USERNAME = "TheCyberfranky"
 
 FORCE_CHANNEL = "franky_backup"
-PAYMENT_CHANNEL = "@franky_payment"   # <--- This is your payment channel
-PAYMENT_UPI = "UPI_QR_Code_Image_URL" # <-- Add your QR image URL here, or just leave as is
-
+PAYMENT_CHANNEL = "@franky_payment"
 NORMAL_DAILY_LIMIT = 10
 PREMIUM_DAILY_LIMIT = 9999
-NORMAL_BULK_LIMIT = 1
-PREMIUM_BULK_LIMIT = 5
-
 AUTO_DELETE_TIME = 1800  # seconds
