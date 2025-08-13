56fba1fc28f25fbf9814862a25a06e2a755a0335
vanshkhandekar <148205080+vanshkhandekar@users.noreply.github.com>
2025-08-13 17:20:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,16 @@
 API_ID = 21609438
 API_HASH = "c80da423ef15ddc47cec79c6b31a6f85"
-
-# Bot Token from BotFather:
 BOT_TOKEN = "8344355788:AAGUD0Bbg5I73yvPvmDBHeEuSNjVoPwZJzA"
-
-# Owner details
 OWNER_ID = 8351290270
 OWNER_USERNAME = "TheCyberfranky"
 
-# Force Join Channel
-FORCE_CHANNEL = "franky_backup"         # Channel username WITHOUT @
+FORCE_CHANNEL = "franky_backup"
+PAYMENT_CHANNEL = "@franky_payment"   # <--- This is your payment channel
+PAYMENT_UPI = "UPI_QR_Code_Image_URL" # <-- Add your QR image URL here, or just leave as is
 
-# Payment channel invite link
-PAYMENT_CHANNEL = "https://t.me/+Qm3-BGlYL0I5ZWM1"
-
-# Limits
 NORMAL_DAILY_LIMIT = 10
 PREMIUM_DAILY_LIMIT = 9999
 NORMAL_BULK_LIMIT = 1
 PREMIUM_BULK_LIMIT = 5
 
-AUTO_DELETE_TIME = 1800  # seconds (30 min)
+AUTO_DELETE_TIME = 1800  # seconds
